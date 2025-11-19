ed667f72c45061a40fc6f9ee96a9d48b9d79f296
JorgeFlowers12 <jorge.efi123@gmail.com>
2025-11-18 22:25:27 -0300

    Agregué comentario de prueba

--- src/main/java/com/example/test2/HelloApplication.java
+++ src/main/java/com/example/test2/HelloApplication.java
@@ -17,7 +17,7 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
 
-        /// HOOLAAAAAA ///////////////////////////////////////////////////////Jorge/////////////////////////
+        /// HOOLAAAAAA //////////////////////////////////////////////////////Jorge/////////////////////////
         /// joakiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiin
         ///  Héeeeeeeeectoooooooooooooooor
         /// joooooooooooooooooooooooooooorgiiiiiiiiiiiiiiiiiitooooooooooooooooooooo
